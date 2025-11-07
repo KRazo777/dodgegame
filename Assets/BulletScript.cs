1bfbddd6ec1d1f39c70d2c48fa6208a17f1fc2b2
kevin razo <recklezzninja11@gmail.com>
2025-11-06 23:09:40 -0800

    Bullet implementation + deflecting physics

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 mousePosition;
+    private Camera mainCamera;
+    private Rigidbody2D rb;
+    public float force = 3f;
+
+    private int bounceCount = 0;
+    public int maxBounces = 5; 
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        rb = GetComponent<Rigidbody2D>();
+
+        Vector3 direction = mousePosition - transform.position;
+        Vector3 rotation = transform.position - mousePosition;
+ 
+        rb.linearVelocity = new Vector2(direction.x, direction.y).normalized * force;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb.linearVelocity != Vector2.zero)
+        {
+            // Get direction it's currently moving
+            Vector2 direction = rb.linearVelocity.normalized;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            // set bullet's rotation to match
+            transform.rotation = Quaternion.Euler(0f, 0f, angle - 90f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        bounceCount++;
+
+        if (bounceCount >= maxBounces)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
