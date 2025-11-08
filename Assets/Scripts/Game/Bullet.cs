85ff7314e6be55f0406611f9fefcfd2a4841f5bf
Taahh <thepheonixblader@gmail.com>
2025-11-07 21:31:12 -0800

    ...

--- Assets/Scripts/Game/Bullet.cs
+++
@@ -1,9 +0,0 @@
-public class Bullet : Entity
-{
-    public readonly string OwnerUniqueId;
-
-    public Bullet(string uniqueId, string ownerUniqueId, EntityType entityType) : base(uniqueId, entityType)
-    {
-        OwnerUniqueId = ownerUniqueId;
-    }
-}
