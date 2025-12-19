b87b511a2bd95f1ca75cc1d4c1e31539e1269d15
kevin razo <recklezzninja11@gmail.com>
2025-12-19 15:21:06 -0800

    Added Different colored for each player in games

--- Assets/Scripts/PrefabHolder.cs
+++ Assets/Scripts/PrefabHolder.cs
@@ -4,4 +4,11 @@ public class PrefabHolder : MonoBehaviour
 {
     public GameObject playerPrefab;
     public GameObject bulletPrefab;
+
+    // array to hold different colored bullet sprites
+    // Element 0 = Red (Default)
+    // Element 1 = Blue
+    // Element 2 = Green
+    // Element 3 = Yellow
+    public Sprite[] bulletSprites; 
 }
